f59a245552a8432f6b3b0548f0464ca8c2849a60
Melik Yunus <melikact34@gmail.com>
2025-09-13 04:43:23 +0300

    authentication

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', include('relationship_app.urls')),  # Include URLs from relationship_app
    # path('admin/', admin.site.urls),
     path('', include('relationship_app.urls')),
+    
 ]
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,7 +3,27 @@ from django.urls import path
 from .views import list_books                 # required literally by checker
 from .views import LibraryDetailView          # CBV import
 
+
+from django.urls import path
+from .views import ( 
+    list_books,
+    LibraryDetailView,
+    register_view,
+    login_view,
+    logout_view
+)
+
 urlpatterns = [
     path('books/', list_books, name='list_books'),  # Function-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # CBV
+
+
+    # Book and Library Views
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+    # Authentication URLs
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
