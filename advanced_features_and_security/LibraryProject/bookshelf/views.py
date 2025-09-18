2fd2a0b4afc733e84160d8aa17073d0315750fdd
Melik Yunus <melikact34@gmail.com>
2025-09-18 08:44:56 +0300

    commiting permision

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -16,3 +16,44 @@ class BookAdmin(admin.ModelAdmin):
 
 # Register the model with the custom admin
 admin.site.register(Book, BookAdmin)
+
+
+
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from .models import Book, Author
+
+# ---- Create Book ----
+# Only users with the 'can_create' permission can access this view
+@permission_required('relationship_app.can_create')
+def add_book(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author_id = request.POST.get('author')
+        publication_year = request.POST.get('publication_year')
+        author = get_object_or_404(Author, id=author_id)
+        Book.objects.create(title=title, author=author, publication_year=publication_year)
+        return redirect('list_books')
+    return render(request, 'relationship_app/add_book.html')
+
+# ---- Edit Book ----
+# Only users with the 'can_edit' permission can access this view
+@permission_required('relationship_app.can_edit')
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.title = request.POST.get('title')
+        book.publication_year = request.POST.get('publication_year')
+        book.save()
+        return redirect('list_books')
+    return render(request, 'relationship_app/edit_book.html', {'book': book})
+
+# ---- Delete Book ----
+# Only users with the 'can_delete' permission can access this view
+@permission_required('relationship_app.can_delete')
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('list_books')
+    return render(request, 'relationship_app/delete_book.html', {'book': book})
